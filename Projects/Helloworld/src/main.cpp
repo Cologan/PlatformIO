01a4a1ebeb28efb8dd52ec3c8a2f13ca3816375f
karanjit96 <karanjit96@gmail.com>
2022-03-16 21:33:24 +0100

    new

---
+++ Projects/Helloworld/src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+#include <iostream>
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  std::cout<<"Hello World"<<std::endl;
+  delay(5000);
+
+}
