775228ada51db2578cf1d9e0ebfdd7c5461a472c
karanjit96 <karanjit96@gmail.com>
2022-03-25 10:28:36 +0100

    update adc

--- Projects/Lab3_ADC/src/main.cpp
+++ Projects/Lab3_ADC/src/main.cpp
@@ -4,6 +4,7 @@
 #define adc_create_pin D7
 #define adc_read_pin A0
 
+float adc_read_value = 0;
 
 // Initialize the OLED display using Arduino Wire:
 SSD1306Wire display(0x3c, SDA, SCL); 
@@ -21,12 +22,39 @@ void setup() {
 
   //set-up the pin for writing
   pinMode(adc_create_pin, OUTPUT);
-  pinMode(adc_read_pin, INPUT);
+ 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.println("Inside loop main");
 
-  display.drawString(0,0,"Hello World from Oled");    //Text to print 
-  display.display();                                  //printing on display
+  for(int Duty_cycle = 0; Duty_cycle < 10; Duty_cycle++){
+    Serial.println("Inside for loop 1");   
+    analogWrite(adc_create_pin, Duty_cycle);
+    adc_read_value=analogRead(adc_read_pin)*0.0032;
+    display.clear();
+    display.drawString(0,0,"ADC Value is: ");           //Text to print 
+    display.drawString(0,10,String(adc_read_value));           //Text to print 
+    display.drawString(0,20,"V");           //Text to print 
+    display.display();
+    Serial.println(adc_read_value);
+    delay(1000);
+  }
+  Serial.println("for loop 1 end");   
+
+
+  for(int Duty_cycle = 10; Duty_cycle > 1; Duty_cycle--){
+    Serial.println("Inside for loop 2"); 
+    analogWrite(adc_create_pin, Duty_cycle);
+    adc_read_value=(adc_read_pin)* 0.0032;
+    display.clear();
+    display.drawString(0,0,"ADC Value is: ");           //Text to print 
+    display.drawString(0,10,String(adc_read_value));           //Text to print 
+    display.drawString(0,20,"V");           //Text to print 
+    display.display();
+    Serial.println(adc_read_value);
+    delay(1000);
+  }
+  Serial.println("for loop 2 end");   
 }
