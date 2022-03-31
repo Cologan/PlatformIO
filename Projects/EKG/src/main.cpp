5c2d94f74b92464f0fddbc565b7d30a32cc2b3da
Cologan <lanfy.dl@gmail.com>
2022-03-31 17:47:21 +0200

    new

--- Projects/EKG/src/main.cpp
+++ Projects/EKG/src/main.cpp
@@ -1,11 +1,32 @@
 #include <Arduino.h>
 #include <SSD1306.h>
+#include<ESP8266TimerInterrupt.h>
+#include <ESP8266_ISR_Timer.h>
+#define USING_TIM_DIV1                false           // for shortest and most accurate timer
+#define USING_TIM_DIV16               true           // for medium time and medium accurate timer
+#define USING_TIM_DIV256              false            // for longest timer but least accurate. Default
+
+#include "ESP8266TimerInterrupt.h"
+#include "ESP8266_ISR_Timer.h"
+
+// Init ESP8266 timer 1
+ESP8266Timer ITimer;
+
+void IRAM_ATTR TimerHandler()
+{
+ISR_timer.run();
+}
+
+#define HW_TIMER_INTERVAL_MS 50L
 
 int loPlus = D6;
 int loMinus = D5;
 int ECGout = A0;
 
-timer1_
+void InterruptFunction()
+{
+
+}
 
 SSD1306Wire display(0x3c, SDA, SCL);  //Setup display connection
 
@@ -16,9 +37,13 @@ Serial.begin(115200); // initialize the serial communication
 pinMode(loPlus, INPUT); // Setup for leads off detection LO +
 pinMode(loMinus, INPUT); // Setup for leads off detection LO –
 
+//Interval in microsecs
+ITimer.attachInterruptInterval(HW_TIMER_INTERVAL_MS*1000,TimerHandler);
+
 display.init(); //initialise display
 display.clear();  //clear display
 display.flipScreenVertically(); //fliiping for ease of use
+timer1_attachInterrupt(onTimerISR);
 
 }
 
